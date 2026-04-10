1b9501198c0c3ac40e342103cb0b600d5abd1009
MOHAMMAD MUZZAMIL <mohammadmuzzamil7770@gmail.com>
2026-04-10 19:44:11 +0500

    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-backend-api-production-1ff1.up.railway.app/api',
+  apiUrl: 'https://muzzamil-portfolio.up.railway.app/api',
   appVersion: '1.0.0'
 };
